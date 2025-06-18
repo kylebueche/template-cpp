17d3a16e67a4dbc7eb54f24d19b85dc35d7cdc6f
Kyle Bueche <kyle.bueche@gmail.com>
2025-06-17 21:19:51 -1000

    Adapting C template to C++

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************
+ * Author: Kyle Bueche
+ * File: main.cpp
+ * 
+ * Simple Hello World
+ ******************************************************/
+#include <iostream>
+#include "twotimestwo.h"
+
+int main()
+{
+	char c = 'i';
+	std::cout << "Hello World!";
+	std::cin >> c;
+	return 0;
+}
---
+++ src/twotimestwo.cpp
@@ -0,0 +1,13 @@
+/********************************************
+ * Author: Kyle Bueche
+ * File: twotimestwo.cpp
+ *
+ * Has a function that multiplies 2 by 2
+ *******************************************/
+
+#include "twotimestwo.h"
+
+int twotimestwo()
+{
+	return 2 * 2;
+}
